5240406ca6b31ea8c583dbc194a08eb15faaaa81
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2025-06-07 16:38:15 -0600

    Listo para trabajar el proyecto

--- src/main/java/sysSolutions/Main.java
+++ src/main/java/sysSolutions/Main.java
@@ -7,6 +7,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello in sysconsulta!");
+        System.out.print("Good afternoon in sysconsulta!");
 
 
     }
